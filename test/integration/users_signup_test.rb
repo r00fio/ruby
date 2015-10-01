185b0f937e435c29f9268aa950d0d4d52f1ee810
r00fio <ugrybkov@gmail.com>
2015-10-01 22:28:28 +0300

    added excercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,22 +5,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path user: {
-                         name: "",
-                         email: "user@invalid",
-                         password: "foo",
-                         password_confirmation: "bar"
-                     }
+                          name: "",
+                          email: "user@invalid",
+                          password: "foo",
+                          password_confirmation: "bar"
+                      }
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.alert"
   end
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:              "password",
-                                            password_confirmation: "password" }
+      post_via_redirect users_path, user: {name: "Example User",
+                                           email: "user@example.com",
+                                           password: "password",
+                                           password_confirmation: "password"}
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
